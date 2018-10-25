a58bf59d839cecb916c2c35e98b4db9b636541a5
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2018-10-25 03:42:03 -0400

    Update src.py

--- src.py
+++ src.py
@@ -15,36 +15,29 @@ def main():
 	adam = optimizers.Adam(lr = 1e-4)   # best run 1e-4 // defaul: 1e-3
 	model_unet.compile(loss = 'binary_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-#	model.summary() # Model summary
+	model.summary() # Model summary
 
 	x_test = prepare_test_data('./test/')
 
 	##### Run model
 	filepath = 'weights.best.hdf5'
-	checkpoint = ModelCheckpoint(filepath, 
-		monitor='val_acc', 
-		verbose=1, 
-		save_best_only=True, 
-		mode='max')
+	checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, 
+		                     save_best_only=True, mode='max')
 	callbacks_list = [checkpoint]
-	model_unet.fit(x_train, y_train, 
-		epochs=2, 
-		batch_size=64, 
-		validation_data=(x_val,y_val), 
-		callbacks=callbacks_list, 
-		verbose=1)
+	model_unet.fit(x_train, y_train, epochs=2, batch_size=64, validation_data=(x_val,y_val), 
+		       callbacks=callbacks_list, verbose=1)
 
 
 	model_unet.load_weights('weights.best.hdf5')
 
-	print('Predictions for validation images')
 	##### Predict validations results
+	print('Predictions for validation images')
 	y_train_pred = model_unet.predict(x_train, verbose=1)
 	y_val_pred = model_unet.predict(x_val, verbose=1)
 
 
+	##### Predict test results
 	print('Predictions for test images')
-	##### Predict validations results
 	x_test_pred = model_unet.predict(x_test, verbose=1)
 	x_test_final = np.round(x_test_pred[:,:,:,0]+ 0.0)
 
