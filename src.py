f7bafae3e621f22c3e7e378daec8879d39759af5
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2018-10-25 03:45:02 -0400

    Update src.py

--- src.py
+++ src.py
@@ -9,15 +9,11 @@ from keras.callbacks import ModelCheckpoint
 
 def main():
 
-	x_train, y_train, x_val, y_val = prepare_training_data('./train/images/', './train/masks/')
-
+	##### Model summary
 	model_unet = unet.unet_model()
 	adam = optimizers.Adam(lr = 1e-4)   # best run 1e-4 // defaul: 1e-3
 	model_unet.compile(loss = 'binary_crossentropy', optimizer=adam, metrics=['accuracy'])
-
-	model.summary() # Model summary
-
-	x_test = prepare_test_data('./test/')
+	model.summary()
 
 	##### Run model
 	filepath = 'weights.best.hdf5'
@@ -26,18 +22,19 @@ def main():
 	callbacks_list = [checkpoint]
 	model_unet.fit(x_train, y_train, epochs=2, batch_size=64, validation_data=(x_val,y_val), 
 		       callbacks=callbacks_list, verbose=1)
-
-
 	model_unet.load_weights('weights.best.hdf5')
 
+	
 	##### Predict validations results
 	print('Predictions for validation images')
+	x_train, y_train, x_val, y_val = prepare_training_data('./train/images/', './train/masks/')
 	y_train_pred = model_unet.predict(x_train, verbose=1)
 	y_val_pred = model_unet.predict(x_val, verbose=1)
 
 
 	##### Predict test results
 	print('Predictions for test images')
+	x_test = prepare_test_data('./test/')
 	x_test_pred = model_unet.predict(x_test, verbose=1)
 	x_test_final = np.round(x_test_pred[:,:,:,0]+ 0.0)
 
